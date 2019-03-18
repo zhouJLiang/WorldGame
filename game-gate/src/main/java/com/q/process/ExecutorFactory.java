5df5a54a94d853c7a0a623e5f7151bb8644b32d4
zhou <280208673@qq.com>
2019-03-18 18:37:58 +0800

    逻辑线程池处理

--- game-gate/src/main/java/com/q/process/ExecutorFactory.java
+++ game-gate/src/main/java/com/q/process/ExecutorFactory.java
@@ -1,5 +1,20 @@
 package com.q.process;
 
-public class ExecutorFactory {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class ExecutorFactory {
+    private Map<String, ExecutorService> threadPoolMap = new ConcurrentHashMap<String, ExecutorService>();
+
+    public enum ExecutorType {
+        Logic_main,
+    }
+
+    public void initThreadPool() {
+        for (ExecutorType type : ExecutorType.values()) {
+            //Executors.newScheduledThreadPool()
+        }
+    }
 }
